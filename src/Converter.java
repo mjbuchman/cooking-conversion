a9d590e4b1d404447bbc434312e2145f4f51ac50
Michael Buchman <mbookie213@gmail.com>
2018-08-09 11:35:56 -0500

    Fixed layout to not read in text from box 2

--- src/Converter.java
+++ src/Converter.java
@@ -1,23 +1,21 @@
 public class Converter {
 	
    private String measurement1, measurement2;
-   private int numberField1, numberField2;
+   private int numberField;
 
    /**
     * Constructor for instantiating a job
     * @param measurement1: The label chosen from the first drop down box
     * @param measurement2: The label chosen from the second drop down box
-    * @param numberField1: The number typed into the first text box
-    * @param numberField2: The number typed into the second text box
+    * @param numberField: The number typed into the text box
     */
-   public Converter(String m1, String m2, int nF1, int nF2) {
+   public Converter(String m1, String m2, int nF) {
        measurement1 = m1;
        measurement2 = m2;
-       numberField1 = nF1;
-       numberField2 = nF2;
+       numberField = nF;
    }
 	
    public void print() {
-		System.out.println("Converting " + this.numberField1 + " " + this.measurement1 + " to " + this.numberField2 + " " + this.measurement2 + "...");
+		System.out.println("Converting " + this.numberField + " " + this.measurement1 + " to " + this.measurement2 + "...");
 	}
 }
