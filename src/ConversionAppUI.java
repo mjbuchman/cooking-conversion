745ae220fb6de4ac8ac192e32e2a0ece2b56417c
Michael Buchman <mbookie213@gmail.com>
2018-08-09 10:36:00 -0500

    Moved test methods to new class

--- src/ConversionAppUI.java
+++ src/ConversionAppUI.java
@@ -1,4 +1,5 @@
 import java.awt.EventQueue;
+import java.lang.*;
 
 import javax.swing.JFrame;
 import javax.swing.JButton;
@@ -10,12 +11,12 @@ import javax.swing.JTextField;
 public class ConversionAppUI {
 
 	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JComboBox comboBox;
-	private JComboBox comboBox_1;
-	public static String meas1, meas2, text1, text2; 
-
+	private JComboBox comboBox1;
+	private JComboBox comboBox2;
+	private JTextField textField1;
+	private JTextField textField2;
+	public static String m1, m2;  // Variables to send both measurement labels to the constructor
+	public static int text1, text2;  // Variables to send both measurement numbers to the constructor
 
 	/**
 	 * Launch the application.
@@ -52,7 +53,13 @@ public class ConversionAppUI {
 		JButton btnNewButton = new JButton("Convert");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				testMethod();
+				m1 = (String) comboBox1.getSelectedItem();
+				m2 = (String) comboBox2.getSelectedItem();
+				text1 = Integer.parseInt(textField1.getText());
+				text2 = Integer.parseInt(textField2.getText());
+				
+				Converter result = new Converter(m1, m2, text1, text2);
+				result.print();
 			}
 		});
 		btnNewButton.setBounds(228, 279, 137, 33);
@@ -61,31 +68,23 @@ public class ConversionAppUI {
 		
 		String[] measurements = new String[] {"Tablespoon", "Teaspoon", "Cup", "Gallon"};
 		
-		comboBox = new JComboBox(measurements);
-		comboBox.setBounds(265, 143, 162, 33);
-		frame.getContentPane().add(comboBox);
+		comboBox1 = new JComboBox(measurements);
+		comboBox1.setBounds(265, 143, 162, 33);
+		frame.getContentPane().add(comboBox1);
 		
-		comboBox_1 = new JComboBox(measurements);
-		comboBox_1.setBounds(265, 198, 162, 33);
-		frame.getContentPane().add(comboBox_1);
+		comboBox2 = new JComboBox(measurements);
+		comboBox2.setBounds(265, 198, 162, 33);
+		frame.getContentPane().add(comboBox2);
+
+		textField1 = new JTextField();
+		textField1.setBounds(166, 143, 64, 33);
+		frame.getContentPane().add(textField1);
+		textField1.setColumns(10);
 		
-		textField = new JTextField();
-		textField.setBounds(166, 143, 64, 33);
-		frame.getContentPane().add(textField);
-		textField.setColumns(10);
-		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(166, 198, 64, 33);
-		frame.getContentPane().add(textField_1);
-	}
-	
-	public void testMethod() {
-		meas1 = (String) comboBox.getSelectedItem();
-		meas2 = (String) comboBox_1.getSelectedItem();
-		text1 = textField.getText();
-		text2 = textField_1.getText();
-		System.out.println("Converting " + text1 + " " + meas1 + " to " + text2 + " " + meas2 + "...");
+		textField2 = new JTextField();
+		textField2.setBounds(166, 198, 64, 33);
+		frame.getContentPane().add(textField2);
+		textField2.setColumns(10);
 	}
 }
 
