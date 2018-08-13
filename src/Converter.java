e674e921c2e4347d2a69cfc3e91732782aeeecca
Michael Buchman <mbookie213@gmail.com>
2018-08-13 14:57:04 -0500

    Added output to application window

--- src/Converter.java
+++ src/Converter.java
@@ -1,123 +1,49 @@
-import java.awt.EventQueue;
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-import javax.swing.JFrame;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.JComboBox;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
-import javax.swing.JTextArea;
-
-public class ConversionAppUI {
-
-	/* Initialize private variables */
-	private JFrame frame;
-	private JComboBox comboBox1; 
-	private JComboBox comboBox2;
-	private JTextField textField1;
+public class Converter {
 	
-	/* Initialize public variables */
-	public static JTextArea textArea;
-	public static String m1, m2;  // Variables to send both measurement labels to the constructor
-	public static int text1, text2;  // Variables to send both measurement numbers to the constructor
+   private String measurement1, measurement2;
+   private int numberField;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					ConversionAppUI window = new ConversionAppUI();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the application.
-	 */
-	public ConversionAppUI() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {	
-		frame = new JFrame();
-		frame.setBounds(100, 100, 637, 508);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(null);
-		
-		JButton btnNewButton = new JButton("Convert");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				/* Gather all user inputs */
-				m1 = (String) comboBox1.getSelectedItem();
-				m2 = (String) comboBox2.getSelectedItem();
-				text1 = Integer.parseInt(textField1.getText());
-				
-				/* Create converter object and display conversion */
-				Converter result = new Converter(m1, m2, text1);
-				result.print();
-				System.out.println(result.getConversionRate());
-			}
-		});
-		btnNewButton.setBounds(228, 279, 137, 33);
-		frame.getContentPane().add(btnNewButton);
-		
-		
-		ArrayList<String> measurements = createStringArray();
-		
-		comboBox1 = new JComboBox(measurements.toArray());
-		comboBox1.setBounds(265, 143, 162, 33);
-		frame.getContentPane().add(comboBox1);
-		
-		comboBox2 = new JComboBox(measurements.toArray());
-		comboBox2.setBounds(265, 198, 162, 33);
-		frame.getContentPane().add(comboBox2);
-
-		textField1 = new JTextField();
-		textField1.setHorizontalAlignment(SwingConstants.CENTER);
-		textField1.setBounds(166, 143, 64, 33);
-		frame.getContentPane().add(textField1);
-		textField1.setColumns(10);
-		
-		textArea = new JTextArea();
-		textArea.setBounds(166, 198, 64, 33);
-		frame.getContentPane().add(textArea);
-	}
+   /**
+    * Constructor for instantiating a job
+    * @param measurement1: The label chosen from the first drop down box
+    * @param measurement2: The label chosen from the second drop down box
+    * @param numberField: The number typed into the text box
+    */
+   public Converter(String m1, String m2, int nF) {
+       measurement1 = m1;
+       measurement2 = m2;
+       numberField = nF;
+   }
 	
-	private ArrayList<String> createStringArray() {
-        String fileName = "labels.txt";
-        String line = null;
-        ArrayList<String> result = new ArrayList<>();
-
-        try {
-            FileReader fileReader = new FileReader(fileName);
-            BufferedReader bufferedReader =  new BufferedReader(fileReader);
-
-            while((line = bufferedReader.readLine()) != null) {
-                result.add(line);
-            }   
-        
-            bufferedReader.close();         
-        }
-        catch(FileNotFoundException ex) {
-            System.out.println("Unable to open file '" + fileName + "'");                
-        }
-        catch(IOException ex) {
-            System.out.println("Error reading file '" + fileName + "'");                  
-        }
-        
-		return result;
+   public double getConversionRate(){	   
+       try {
+    	   File file = new File("conversions.txt");
+    	   Scanner sc = new Scanner(file);
+       
+    	   while(sc.hasNextLine()){
+    		  if(sc.next().equals(this.measurement1)) {
+    			  if(sc.next().equals(this.measurement2)){
+    				  return sc.nextDouble();
+    			  }
+    		  }
+    	   }
+           sc.close();         
+       }
+       catch(Exception ex) {
+           ex.printStackTrace();               
+       }
+	   
+	   return 0.0;  // Used for error checking
+   }
+   
+   public double getResult(double rate) {
+	   return ((double) Math.round(this.numberField*rate*1000))/1000;
+   }
+   
+   public String toString(double rate) {
+		return "" + rate;
 	}
 }
-
