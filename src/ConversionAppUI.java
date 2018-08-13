c399e0b1cb443696882bc03592d5e3a01de9d2ed
Michael Buchman <mbookie213@gmail.com>
2018-08-13 14:56:25 -0500

    Added output to application window

--- src/ConversionAppUI.java
+++ src/ConversionAppUI.java
@@ -6,10 +6,8 @@ import javax.swing.JFrame;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-import javax.swing.JComboBox;
-import javax.swing.JTextField;
-import javax.swing.SwingConstants;
-import javax.swing.JTextArea;
+import javax.swing.*;
+import java.awt.*;
 
 public class ConversionAppUI {
 
@@ -19,10 +17,9 @@ public class ConversionAppUI {
 	private JComboBox comboBox2;
 	private JTextField textField1;
 	
-	/* Initialize public variables */
-	public static JTextArea textArea;
+	public static JLabel lblAns;
 	public static String m1, m2;  // Variables to send both measurement labels to the constructor
-	public static int text1, text2;  // Variables to send both measurement numbers to the constructor
+	public static int text1;  // Variables to send both measurement numbers to the constructor
 
 	/**
 	 * Launch the application.
@@ -56,7 +53,7 @@ public class ConversionAppUI {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JButton btnNewButton = new JButton("Convert");
+		JButton btnNewButton = new JButton("Calculate");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				/* Gather all user inputs */
@@ -67,32 +64,53 @@ public class ConversionAppUI {
 				/* Create converter object and display conversion */
 				Converter result = new Converter(m1, m2, text1);
 				double rate = result.getConversionRate();
-				System.out.println(result.getResult(rate));
+				lblAns.setText(result.toString(result.getResult(rate)));
 			}
 		});
-		btnNewButton.setBounds(228, 279, 137, 33);
+		btnNewButton.setBounds(140, 279, 137, 33);
 		frame.getContentPane().add(btnNewButton);
 		
 		
 		ArrayList<String> measurements = createStringArray();
 		
 		comboBox1 = new JComboBox(measurements.toArray());
-		comboBox1.setBounds(265, 143, 162, 33);
+		comboBox1.setBounds(191, 143, 162, 33);
 		frame.getContentPane().add(comboBox1);
 		
 		comboBox2 = new JComboBox(measurements.toArray());
-		comboBox2.setBounds(265, 198, 162, 33);
+		comboBox2.setBounds(191, 198, 162, 33);
 		frame.getContentPane().add(comboBox2);
 
 		textField1 = new JTextField();
 		textField1.setHorizontalAlignment(SwingConstants.CENTER);
-		textField1.setBounds(166, 143, 64, 33);
+		textField1.setBounds(89, 143, 64, 33);
 		frame.getContentPane().add(textField1);
 		textField1.setColumns(10);
 		
-		textArea = new JTextArea();
-		textArea.setBounds(166, 198, 64, 33);
-		frame.getContentPane().add(textArea);
+		JLabel lblConvert = new JLabel("Convert");
+		lblConvert.setFont(new Font("Tahoma", Font.BOLD, 16));
+		lblConvert.setHorizontalAlignment(SwingConstants.CENTER);
+		lblConvert.setBounds(171, 79, 70, 43);
+		frame.getContentPane().add(lblConvert);
+		
+		JLabel lblTo = new JLabel("to");
+		lblTo.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblTo.setHorizontalAlignment(SwingConstants.CENTER);
+		lblTo.setBounds(95, 207, 46, 14);
+		frame.getContentPane().add(lblTo);
+		
+		JLabel lblResult = new JLabel("Result");
+		lblResult.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblResult.setBounds(497, 145, 70, 24);
+		frame.getContentPane().add(lblResult);
+		
+		lblAns = new JLabel("");
+		lblAns.setOpaque(true);
+		lblAns.setHorizontalAlignment(SwingConstants.CENTER);
+		lblAns.setBackground(Color.WHITE);
+		lblAns.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		lblAns.setBounds(497, 196, 46, 33);
+		frame.getContentPane().add(lblAns);
 	}
 	
 	private ArrayList<String> createStringArray() {
