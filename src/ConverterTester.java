9384a7cf0840806ca1605555fa3aeaa6843fd10d
Michael Buchman <mbookie213@gmail.com>
2018-09-06 15:26:43 -0500

    Added user input to improve test speed

--- src/ConverterTester.java
+++ src/ConverterTester.java
@@ -1,6 +1,20 @@
+import java.util.*;
+
 public class ConverterTester {
 	public static void main(String[] args) {
-		Converter test = new Converter("Tablespoon(s)", "Cup(s)", 8);
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter first measurement in singular form (i.e. Tablespoon): ");
+		String a = input.nextLine();
+		
+		System.out.print("Enter second measurement in singular form (i.e. Tablespoon): ");
+		String b = input.nextLine();
+		
+		System.out.print("Enter number of " + a + "s to convert to " + b + "s: ");
+		int c = input.nextInt();
+		
+		input.close();
+		
+		Converter test = new Converter(a + "(s)", b + "(s)", c);
 		System.out.println("Conversion Rate: " + test.getConversionRate());
 		System.out.println("Result: " + test.getResult(test.getConversionRate()));
 	}
