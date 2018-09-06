0ba586bd8af43fccec6b3f743d7fd12b0dee5033
Michael Buchman <mbookie213@gmail.com>
2018-09-06 15:13:28 -0500

    Added test case and printed results

--- src/ConverterTester.java
+++ src/ConverterTester.java
@@ -1,1 +1,7 @@
-
+public class ConverterTester {
+	public static void main(String[] args) {
+		Converter test = new Converter("Tablespoon(s)", "Cup(s)", 8);
+		System.out.println("Conversion Rate: " + test.getConversionRate());
+		System.out.println("Result: " + test.getResult(test.getConversionRate()));
+	}
+}
